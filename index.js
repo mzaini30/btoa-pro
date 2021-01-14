fbb7a53c0bb3427923437ce8ae65d1e6a5fbf938
mzaini30 <muhzaini30@gmail.com>
2021-01-14 21:27:07 +0800

    oke

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
+/*
 const btoa = require("btoa")
 module.exports = function btoaPro(x){
 	return btoa(btoa(encodeURIComponent(x)))
 }
+*/
+import btoa from "btoa"
+const btoaPro = x => btoa(btoa(encodeURIComponent(x)))
+export default btoaPro
